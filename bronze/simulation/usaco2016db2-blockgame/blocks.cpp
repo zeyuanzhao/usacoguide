f54d8f73d8f70d303420e167b63756674c3175b7
tenderpork <14340342+zeyuanzhao@users.noreply.github.com>
2023-06-21 02:06:30 +0000

    asdf

--- bronze/simulation/usaco2016db2-blockgame/blocks.cpp
+++ bronze/simulation/usaco2016db2-blockgame/blocks.cpp
@@ -3,8 +3,8 @@ using namespace std;
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    // freopen("blocks.in", "r", stdin);
-    // freopen("blocks.out", "w", stdout);
+    freopen("blocks.in", "r", stdin);
+    freopen("blocks.out", "w", stdout);
 
     int n; cin >> n;
     unordered_map<char, int> answer;
