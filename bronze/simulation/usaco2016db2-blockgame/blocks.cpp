364c527886371ad9b85b893327a689d7287909a0
tenderpork <14340342+zeyuanzhao@users.noreply.github.com>
2023-06-21 02:05:56 +0000

    blockgame

--- bronze/simulation/usaco2016db2-blockgame/blocks.cpp
+++ bronze/simulation/usaco2016db2-blockgame/blocks.cpp
@@ -1,11 +1,39 @@
 #include <bits/stdc++.h>
-        using namespace std;
+using namespace std;
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    // freopen("blocks.in", "r", stdin);
+    // freopen("blocks.out", "w", stdout);
 
-        int main() {
-            ios_base::sync_with_stdio(false);
-            cin.tie(NULL);
+    int n; cin >> n;
+    unordered_map<char, int> answer;
+    for (char c = 'a'; c <= 'z'; c++) {
+        answer[c] = 0;
+    }
 
-            // freopen("blocks.in", "r", stdin);
-            // freopen("blocks.out", "w", stdout);
-
+    unordered_map<char, int> aletters;
+    unordered_map<char, int> bletters;
+    for (int i = 0; i < n; i++) {
+        for (char c = 'a'; c <= 'z'; c++) {
+            aletters[c] = 0;
         }
+        for (char c = 'a'; c <= 'z'; c++) {
+            bletters[c] = 0;
+        }
+        string a, b;
+        cin >> a >> b;
+        for (auto c : a) {
+            aletters[c]++;
+        }
+        for (auto c : b) {
+            bletters[c]++;
+        }
+        for (char c = 'a'; c <= 'z'; c++) {
+            answer[c] += max(aletters[c], bletters[c]);
+        }
+    }
+    for (char c = 'a'; c <= 'z'; c++) {
+        cout << answer[c] << endl;
+    }
+}
