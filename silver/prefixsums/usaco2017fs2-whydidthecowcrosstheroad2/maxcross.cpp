43b9a9b316899413f3b247f00b1a6dfb435daa02
tenderpork <14340342+zeyuanzhao@users.noreply.github.com>
2023-06-22 16:50:09 +0000

    maxcross

--- silver/prefixsums/usaco2017fs2-whydidthecowcrosstheroad2/maxcross.cpp
+++ silver/prefixsums/usaco2017fs2-whydidthecowcrosstheroad2/maxcross.cpp
@@ -5,4 +5,31 @@ int main() {
     cin.tie(NULL);
     // freopen("maxcross.in", "r", stdin);
     // freopen("maxcross.out", "w", stdout);
+
+    int n, k, b;
+    cin >> n >> k >> b;
+
+    vector<int> signals(b);
+    for (int i = 0; i < b; i++) {
+        cin >> signals[i];
+    }
+    sort(signals.begin(), signals.end());
+    signals.push_back(n+1);
+
+    vector<int> prefix(n+1, 0);
+    int run = 1;
+    for (int i = 0; i < b; i++) {
+        for (int j = signals[i]; j < signals[i+1]; j++) {
+            prefix[j] = run;
+        }
+        run++;
+    }
+
+    int ans = n+123;
+    for (int i = 0; i <= n-k; i++) {
+        int dif = prefix[i+k-1] - prefix[i-1];
+        ans = min(ans, dif);
+    }
+    cout << ans;
+
 }
