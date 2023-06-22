e896c1a7ef998db0b97f96deb3e912cfd915cf08
tenderpork <14340342+zeyuanzhao@users.noreply.github.com>
2023-06-22 15:38:38 +0000

    asdf

--- silver/prefixsums/cses-subarraydivisibility/div.cpp
+++ silver/prefixsums/cses-subarraydivisibility/div.cpp
@@ -6,17 +6,17 @@ int main() {
     cin.tie(NULL);
 
     int n; cin >> n;
-    vector<int> arr (n+1);
+    vector<long> arr (n+1);
     for (int i = 1; i <= n; i++) {
         cin >> arr[i];
     }
 
-    unordered_map<int, int> sums;
-    int prefix = 0;
-    int ans = 0;
+    unordered_map<long, int> sums;
+    long prefix = 0;
+    long ans = 0;
     sums[0] = 1;
     for (int i = 1; i <= n; i++) {
-        prefix = (prefix + arr[i]) % n;
+        prefix = ((prefix + arr[i]) % n + n) % n;
         ans += sums[prefix];
         sums[prefix]++;
     }
