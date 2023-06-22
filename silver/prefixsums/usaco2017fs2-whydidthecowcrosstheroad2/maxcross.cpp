7160c6e96f22db2cff0e51e44479eb0811f8d257
tenderpork <14340342+zeyuanzhao@users.noreply.github.com>
2023-06-22 15:15:26 +0000

    ugsetup fix

---
+++ silver/prefixsums/usaco2017fs2-whydidthecowcrosstheroad2/maxcross.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    // freopen("maxcross.in", "r", stdin);
+    // freopen("maxcross.out", "w", stdout);
+}
