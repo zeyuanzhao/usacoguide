07bc12342ed1449ed54c495b2f2aa391d24e5059
tenderpork <14340342+zeyuanzhao@users.noreply.github.com>
2023-06-22 16:58:47 +0000

    asdf

--- silver/prefixsums/usaco2017fs2-whydidthecowcrosstheroad2/maxcross.cpp
+++ silver/prefixsums/usaco2017fs2-whydidthecowcrosstheroad2/maxcross.cpp
@@ -3,8 +3,8 @@ using namespace std;
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    // freopen("maxcross.in", "r", stdin);
-    // freopen("maxcross.out", "w", stdout);
+    freopen("maxcross.in", "r", stdin);
+    freopen("maxcross.out", "w", stdout);
 
     int n, k, b;
     cin >> n >> k >> b;
