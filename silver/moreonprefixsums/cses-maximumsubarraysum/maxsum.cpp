f5a78bd382730db025dc52986ae73a0fc3be93af
tenderpork <14340342+zeyuanzhao@users.noreply.github.com>
2023-06-23 15:29:35 +0000

    asdf

--- silver/moreonprefixsums/cses-maximumsubarraysum/maxsum.cpp
+++ silver/moreonprefixsums/cses-maximumsubarraysum/maxsum.cpp
@@ -4,13 +4,13 @@ using namespace std;
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    // freopen(".in", "r", stdin);
-    // freopen(".out", "w", stdout);
+    freopen("maxsum.in", "r", stdin);
+    // freopen("maxsum.out", "w", stdout);
 
     int n; cin >> n;
     long long prefix = 0;
     long long small = 0;
-    long long ans = LONG_MAX;
+    long long ans = LONG_MIN;
     for (int i = 0; i < n; i++) {
         long num; cin >> num; prefix += num;
         ans = max(ans, prefix-small);
