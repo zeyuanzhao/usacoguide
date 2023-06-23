0ef7b88e83b3e6980ee4a9ce30c98a6b0df3c11e
tenderpork <14340342+zeyuanzhao@users.noreply.github.com>
2023-06-23 15:23:59 +0000

    asdf

--- silver/moreonprefixsums/cses-maximumsubarraysum/maxsum.cpp
+++ silver/moreonprefixsums/cses-maximumsubarraysum/maxsum.cpp
@@ -10,11 +10,11 @@ int main() {
     int n; cin >> n;
     int prefix = 0;
     int small = 0;
-    int large = 0;
+    int ans = INT_MIN;
     for (int i = 0; i < n; i++) {
         int num; cin >> num; prefix += num;
+        ans = max(ans, prefix-small);
         small = min(small, prefix);
-        large = max(large, prefix);
     }
-    cout << large - small;
+    cout << ans;
 }
