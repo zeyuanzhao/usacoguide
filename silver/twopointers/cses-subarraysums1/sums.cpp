c50f53a6752c0d4af24ce259db3336a7632b824d
tenderpork <14340342+zeyuanzhao@users.noreply.github.com>
2023-08-24 18:22:00 +0000

    adf

--- silver/twopointers/cses-subarraysums1/sums.cpp
+++ silver/twopointers/cses-subarraysums1/sums.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    freopen("sums.in", "r", stdin);
+    // freopen("sums.in", "r", stdin);
     // freopen("sums.out", "w", stdout);
 
     int n, x;
